0c7cf6d9072701478ac2450de86493b38e0d5a7a
hyeongjin kim <hyeongjinkim@hyeongjins-MacBook-Pro.local>
2018-06-11 10:34:12 +0900

    sign-up error text

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    # User.find(1)과 같은 의미임. find는 정수로 알아서 표현됨.
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                               :password_confirmation)
+    end
 end
